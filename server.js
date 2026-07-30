fd23f15c5b060bb8100c4506e41e6b71ce173160
lynngoh7 <gohilynn@gmail.com>
2026-07-30 11:33:10 +0800

    Stage 1: Signup and login routes working

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
 require('dotenv').config()
 const express = require('express')
 const { supabase } = require('./lib/supabaseClient')
+const { refresh } = require('next/cache')
 
 const app = express()
 app.use(express.json())
 
 const PORT = process.env.PORT || 3000
 
+app.post('/auth/signup', async (req, res) =>{
+  const { email, password } = req.body
+  if(!email || !password) {
+    return res.status(400).json({ error: 'Email and password are required' })
+  }
+
+  const { data, error } = await supabase.auth.signUp({ email, password})
+  if (error) {
+    return res.status(400).json({ error: error.message })
+  }
+
+  return res.status(201).json({user: data.user})
+
+})
+
+app.post('/auth/login', async (req,res) => {
+  const { email, password } = req.body
+  if( !email || !password) {
+    return res.status(400).json({ error: 'Email and password required'})
+  }
+  const { data, error } = await supabase.auth.signInWithPassword({email, password})
+  if (error){
+    return res.status(401).json({ error: 'Invalid login credentials'})
+  }
+  return res.status(200).json({
+    access_token: data.session.access_token,
+    refresh_token: data.session.refresh_token,
+    user: data.user,
+  })
+})
 async function startServer() {
   const { error } = await supabase.auth.getSession()
   if (error) {
